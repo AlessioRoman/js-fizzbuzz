ee7f2a193906d50c35b1ea1a8234297c408c5161
AlessioRoman <alessio.romano02dev@gmail.com>
2023-03-30 16:27:18 +0200

    added colors for boxes

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
     document.getElementById("square-container").innerHTML += `
-    <div class="bg-secondary d-flex justify-content-center align-items-center square rounded-3 mb-3">
+    <div class="bg-primary d-flex justify-content-center align-items-center square rounded-3 mb-3">
         <div class="text-center">
             Fizz Buzz
         </div>
@@ -9,7 +9,7 @@ for (let i = 1; i <= 100; i++) {
     `;
   } else if (i % 3 == 0) {
     document.getElementById("square-container").innerHTML += `
-    <div class="bg-secondary d-flex justify-content-center align-items-center square rounded-3 mb-3">
+    <div class="bg-warning d-flex justify-content-center align-items-center square rounded-3 mb-3">
         <div class="text-center">
             Fizz
         </div>
@@ -17,7 +17,7 @@ for (let i = 1; i <= 100; i++) {
     `;
   } else if (i % 5 == 0) {
     document.getElementById("square-container").innerHTML += `
-    <div class="bg-secondary d-flex justify-content-center align-items-center square rounded-3 mb-3">
+    <div class="bg-danger d-flex justify-content-center align-items-center square rounded-3 mb-3">
         <div class="text-center">
             Buzz
         </div>
